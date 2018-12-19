d014350ac5e1a530d171d567b14131f57a8de4d7
Roman Guivan <Roman.Guivan@mercedes-benz.io>
2018-12-19 15:05:11 +0100

    Here it all starts

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+   <h1> Extreme sports wiki !</h1>
+   <p>Not really a wiki since i'm not using mediawiki engine or a similar alternative </p>
+   <p>I had this urge to document some of the games "inspired" by Tony Hawk's Pro Skater, SSX, Wave Race and other "EXTREME" games. Some of the tiles listed have literally nothing to do with extreme sports, yet their mechanics sort of are. Feel free to put my XTREME criteria into question.</p>
+   <ul>
+      <li><a href="#">Extreme games by mechanics</a></li>
+      <li><a href="#">Extreme games by sport</a></li>
+      <li><a href="#">Extreme games alphabetically</a></li>
+      <li><a href="#">Extreme games alphabetically</a></li>
+   </ul>
+   
+  </Layout>
+)
