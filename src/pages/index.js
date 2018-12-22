9b47fd07f13c1f8a7e03bde22254ffd1691ecd42
RGuivan <guivanrv@gmail.com>
2018-12-22 13:01:10 +0200

    Logo is in

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 
 export default ({data}) => (
   <Layout>
-   <h1> Extreme sports wiki !</h1>
+   <img src="logo.png" style={{ "display" : "flex", "margin" : "auto", "paddingBottom": "10px" }} alt="Extreme sports wiki!"/>
    <p>Not really a wiki since i'm not using mediawiki engine or a similar alternative </p>
    <p>I had this urge to document some of the games "inspired" by Tony Hawk's Pro Skater, SSX, Wave Race and other "EXTREME" games. Some of the tiles listed have literally nothing to do with extreme sports, yet their mechanics sort of are. Feel free to put my XTREME criteria into question.</p>
    <p> So far I have {data.allMarkdownRemark.totalCount} poorly listed, slowly moderating the content myself</p>
