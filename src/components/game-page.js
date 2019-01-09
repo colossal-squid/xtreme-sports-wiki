b8716981552aed960c6560fde4ad665538cb4aad
Roman Guivan <Roman.Guivan@mercedes-benz.io>
2019-01-09 10:20:43 +0100

    Colon issue workaround

--- src/components/game-page.js
+++ src/components/game-page.js
@@ -9,12 +9,12 @@ export default ({data}) => {
     const url = data.site.siteMetadata.url + post.fields.slug;
     return (
       <React.Fragment>
-        <Meta title={post.frontmatter.title}></Meta>
+        <Meta title={post.frontmatter.title.replace('&#x3a;', ':')}></Meta>
         <Layout nav={ {
-          title: post.frontmatter.title,
+          title: post.frontmatter.title.replace('&#x3a;', ':'),
           url: post.fields.slug,
         } }>
-            <h3>{post.frontmatter.title}</h3>
+            <h3>{post.frontmatter.title.replace('&#x3a;', ':')}</h3>
             <div dangerouslySetInnerHTML={{ __html: post.html }} />
             <h3>Comments</h3>
             <FacebookComments enabled={true} url={url}>
